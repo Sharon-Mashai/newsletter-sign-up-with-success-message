899a0d0c826f6bf7ed2a3e194a756a9928763e76
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-06-06 18:56:11 +0200

    Newsletter sign up with message complete project

---
+++ script.js
@@ -0,0 +1,48 @@
+let emailInput = document.getElementById("email");
+let subscribeBtn = document.getElementById("subscribeBtn");
+
+let signupCard = document.getElementById("signupCard");
+let successCard = document.getElementById("successCard");
+
+let userEmail = document.getElementById("userEmail");
+let errorMessage = document.getElementById("errorMessage");
+
+subscribeBtn.addEventListener("click", function(){
+
+    let email = emailInput.value;
+
+    let emailPattern =
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(email === ""){
+        errorMessage.textContent =
+        "Valid email required";
+        return;
+    }
+
+    if(!emailPattern.test(email)){
+        errorMessage.textContent =
+        "Valid email required";
+        return;
+    }
+
+    errorMessage.textContent = "";
+
+    userEmail.textContent = email;
+
+    signupCard.style.display = "none";
+    successCard.style.display = "block";
+
+});
+
+let dismissBtn =
+document.getElementById("dismissBtn");
+
+dismissBtn.addEventListener("click", function(){
+
+    successCard.style.display = "none";
+    signupCard.style.display = "flex";
+
+    emailInput.value = "";
+
+});
